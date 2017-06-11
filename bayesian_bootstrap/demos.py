e48203c08f722e3495cd32676dbe99d937d754bd
lmc2179 <louiscialdella@gmail.com>
2017-06-10 23:43:06 -0700

    added exponential distribution demo

--- bayesian_bootstrap/demos.py
+++ bayesian_bootstrap/demos.py
@@ -9,36 +9,47 @@ def plot_mean_bootstrap():
     X = [-1, 0, 1]
     posterior_samples = mean(X, 10000)
     sns.distplot(posterior_samples)
-    efron_samples = [np.mean(resample(X)) for _ in range(10000)]
-    sns.distplot(efron_samples)
+    classical_samples = [np.mean(resample(X)) for _ in range(10000)]
+    sns.distplot(classical_samples)
     plt.show()
 
 def plot_mean_resample_bootstrap():
     X = [-1, 0, 1]
     posterior_samples = bayesian_bootstrap(X, np.mean, 10000, 100)
     sns.distplot(posterior_samples)
-    efron_samples = [np.mean(resample(X)) for _ in range(10000)]
-    sns.distplot(efron_samples)
+    classical_samples = [np.mean(resample(X)) for _ in range(10000)]
+    sns.distplot(classical_samples)
     plt.show()
 
 def plot_var_bootstrap():
     X = np.random.uniform(-1, 1, 100)
     posterior_samples = var(X, 10000)
     sns.distplot(posterior_samples)
-    efron_samples = [np.var(resample(X)) for _ in range(10000)]
-    sns.distplot(efron_samples)
+    classical_samples = [np.var(resample(X)) for _ in range(10000)]
+    sns.distplot(classical_samples)
     plt.show()
 
 def plot_var_resample_bootstrap():
     X = np.random.uniform(-1, 1, 100)
     posterior_samples = bayesian_bootstrap(X, np.var, 10000, 500)
     sns.distplot(posterior_samples)
-    efron_samples = [np.var(resample(X)) for _ in range(10000)]
-    sns.distplot(efron_samples)
+    classical_samples = [np.var(resample(X)) for _ in range(10000)]
+    sns.distplot(classical_samples)
+    plt.show()
+
+def plot_mean_method_comparison():
+    X = np.random.exponential(scale=1, size=8)
+    classical_samples = [np.mean(resample(X)) for _ in range(10000)]
+    posterior_samples_resample = bayesian_bootstrap(X, np.mean, 10000, 1000)
+    posterior_samples_weighted = mean(X, 10000)
+    sns.distplot(classical_samples)
+    sns.distplot(posterior_samples_resample)
+    sns.distplot(posterior_samples_weighted)
     plt.show()
 
 if __name__ == '__main__':
     # plot_mean_bootstrap()
     # plot_mean_resample_bootstrap()
     # plot_var_bootstrap()
-    plot_var_resample_bootstrap()
+    # plot_var_resample_bootstrap()
+    plot_mean_method_comparison()
