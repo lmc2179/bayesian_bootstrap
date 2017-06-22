ad57e44d2bf62007c5355cd92d69667997a9cd46
lmc2179 <louiscialdella@gmail.com>
2017-06-21 23:13:51 -0700

    added basic setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+    name = "bayesian_bootstrap",
+    packages = ["bayesian_bootstrap"],
+    version = "1.0.0",
+    description = "Bayesian Bootstrapping for statistics and regression models",
+    author = "Louis Cialdella",
+    author_email = "louiscialdella@gmail.com",
+    url = "https://github.com/lmc2179/bayesian_bootstrap",
+    download_url = "https://github.com/lmc2179/bayesian_bootstrap/archive/master.zip",
+    keywords = ["statistics", "bayesian", "machine learning"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering",
+        ],
+    long_description = """"""
+)
