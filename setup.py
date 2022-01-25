85237d9df0a3f5133eff5d2abe533b0f6ab6f3ee
Graham Enos <genos@rigetti.com>
2022-01-25 15:00:56 -0500

    Add install_requires

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from distutils.core import setup
+
+with open("./requirements.txt") as f:
+    REQUIRES = [line.strip() for line in f]
+
 setup(
     name = "bayesian_bootstrap",
     packages = ["bayesian_bootstrap"],
@@ -9,6 +13,7 @@ setup(
     url = "https://github.com/lmc2179/bayesian_bootstrap",
     download_url = "https://github.com/lmc2179/bayesian_bootstrap/archive/master.zip",
     keywords = ["statistics", "bayesian", "machine learning", "bootstrap", "bayes", "probability", "inference"],
+    install_requires=REQUIRES,
     classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
